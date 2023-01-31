81322fa59619c8df3dc07b2a10ba61d5ebaa47de
Navin-Morajkar <navinsika@gmail.com>
2023-01-31 23:43:42 +0530

    DS_FCFS

--- OS_Expts/DiskScheduling_FCFS/src/diskscheduling_fcfs/DiskScheduling_FCFS.java
+++ OS_Expts/DiskScheduling_FCFS/src/diskscheduling_fcfs/DiskScheduling_FCFS.java
@@ -13,31 +13,22 @@ public class DiskScheduling_FCFS {
         nc=sc.nextInt(); 
         
         int requests[]=new int[nc]; 
-        int hm[]=new int[nc]; 
-        
+                
         System.out.println("Enter the cylinder request numbers: "); 
         for(i=0;i<nc;i++) 
             requests[i]=sc.nextInt();
         
         System.out.println("Enter initial head position: "); 
-        initial_head_pos=sc.nextInt(); 
+        initial_head_pos=sc.nextInt();
+        
         cur_track=initial_head_pos;
         
         for(i=0;i<nc;i++) {
-            if(requests[i] > cur_track) { 
-                hm[i]=requests[i]-cur_track; 
-                cur_track = requests[i]; 
-                System.out.println("At Position\t"+cur_track); 
-            } 
-            else {
-                hm[i]=cur_track-requests[i]; 
-                cur_track=requests[i]; 
-                System.out.println("At Position\t"+cur_track); 
-            }
-            
-            total=total+hm[i];
-        } 
-             
+            int headMovement = Math.abs(requests[i] - cur_track);
+            cur_track = requests[i]; 
+            System.out.println("At Position\t"+cur_track); 
+            total += headMovement;
+        }    
         System.out.println("The total head movement: " + total); 
     }
 }
